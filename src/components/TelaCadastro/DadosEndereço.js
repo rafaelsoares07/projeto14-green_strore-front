b1728be2d55cd299402d646d9a86f071b17c6b2a
c.giacometti <camilla.giacometti@gmail.com>
2022-07-07 21:48:36 -0300

    consifgurações iniciais do cadastro

---
+++ src/components/TelaCadastro/DadosEndereço.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export default function DadosEndereço(){
+    return (
+        <InputContainer>
+            <HeaderDados>
+                Endereço
+            </HeaderDados>
+        
+        </InputContainer>
+    );
+}
+
+const InputContainer = styled.div `
+    box-sizing: border-box;
+    width: 50%;
+    padding: 20px;
+    border-right: 1px solid #7CC644;
+`
+const HeaderDados = styled.div `
+    color: #7F7F7F;
+    font-size: 30px;
+    border-bottom: 1px solid #7CC644;
+`
